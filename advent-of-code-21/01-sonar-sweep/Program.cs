660aaf4a749d1f18306cdc9c2cea2b7235575cf8
felkan <felixg.nti@gmail.com>
2021-12-03 17:53:41 +0100

    day 1

---
+++ advent-of-code-21/01-sonar-sweep/Program.cs
@@ -0,0 +1,39 @@
+﻿OneStar();
+TwoStar();
+
+string[] ReadFile()
+{
+    string[] readText = System.IO.File.ReadAllLines(@"input.txt");
+    return readText;
+}
+
+// 1*
+void OneStar()
+{
+    string[] textLines = ReadFile();
+    int increases = 0;
+
+    for (int i = 0; i < textLines.Length - 1; i++)
+    {
+        if (Int32.Parse(textLines[i + 1]) > Int32.Parse(textLines[i]))
+            increases++;
+    }
+
+    Console.WriteLine(increases);
+}
+
+// 2*
+void TwoStar()
+{
+    string[] textLines = ReadFile();
+    int increases = 0;
+
+    for (int i = 0; i < textLines.Length - 3; i++)
+    {
+        if (Int32.Parse(textLines[i + 1]) + Int32.Parse(textLines[i + 2]) + Int32.Parse(textLines[i + 3]) >
+            Int32.Parse(textLines[i]) + Int32.Parse(textLines[i + 1]) + Int32.Parse(textLines[i + 2]))
+            increases++; 
+    }
+
+    Console.WriteLine(increases);
+}
